cd69e612459106f55cf761773940a6a9684ab4db
varunsingh034 <varunbaroliya034@gmail.com>
2025-07-19 16:06:24 +0530

    empty

--- script.js
+++
@@ -1,9 +0,0 @@
-let abc = document.querySelector("#abc")
-
-abc.addEventListener("mouseover", function(dets){
-    abc.style.backgroundColor = "yellow";
-})
-
-abc.addEventListener("mouseout", function(dets){
-    abc.style.backgroundColor = "red";
-})
